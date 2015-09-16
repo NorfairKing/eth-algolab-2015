9acb3569fd386ed95f21b536d02635e8284b2341
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-16 21:46:09 +0200

    implemented a cleverer solution with precomputed sums

--- evenpairs/src/main.cc
+++ evenpairs/src/main.cc
@@ -21,17 +21,25 @@ inline bool even(int n) {
   return (n & 1) ^ 1; // The last bit of n must be 0.
 }
 
-// Go over every pair i,j; compute the sum of the numbers between i and j
-// by adding the jth number for every j and check whether it's even every time.
+// Define s[i] as \sum_{i=0}^n bits[i]
+// For each couple i,j the sum of elements between i and j can be found by:
+// \sum_{n=i}^j bits[i] = s[j]-s[i]
+//
+// For evey pair i and j, compute the sum and check whether it's even.
+// If so, the ncount it.
 int solve(std::vector<int> bits) {
   int n = bits.size();
 
+  std::vector<int> s(n+1);
+  s[0] = bits[0];
+  for (int i = 1; i <= n; ++i) {
+    s[i] = s[i-1] + bits[i-1];
+  }
+
   int even_count = 0;
-  for (int i = 0; i < n; ++i) {
-    int sum = 0;
-    for (int j = i; j < n; ++j) {
-      sum += bits[j];
-      if (even(sum)) { ++even_count; }
+  for (int i = 0; i <= n; ++i) {
+    for (int j = i+1; j <= n; ++j) {
+      if (even(s[j] - s[i])) { ++even_count; }
     }
   }
 
