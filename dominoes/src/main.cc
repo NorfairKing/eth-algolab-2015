1c05de5dff82852a37009f52fbf94142f334b04c
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-16 23:27:11 +0200

    wrote a correct solution to the dominoes problem

---
+++ dominoes/src/main.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+int solve(std::vector<int> stones);
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+
+  int cases;
+  std::cin >> cases;
+  for (int c = 0; c < cases; ++c) {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> stones(n);
+    for (int i = 0; i < n; ++i) {
+      std::cin >> stones[i];
+    }
+
+    std::cout << solve(stones) << std::endl;
+  }
+}
+
+// Keep a counter of the last domino that's definitely going to fall
+// Go over all the dominoes until that counter and make them topple,
+// that means updating the counter to reflect the new last domino.
+int solve(std::vector<int> stones) {
+  int n = stones.size();
+
+  int last = 0;
+  for (int i = 0; i < n; ++i) {
+    if (i > last) { return last + 1; }
+    last = std::max(i + stones[i] - 1, last);
+  }
+  return n;
+}
