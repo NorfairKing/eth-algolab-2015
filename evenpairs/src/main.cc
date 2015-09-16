2fca40608731533a046f2d1bc5fb435c6b07011b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-16 21:58:03 +0200

    implemented a linear-time algorithm

--- evenpairs/src/main.cc
+++ evenpairs/src/main.cc
@@ -25,21 +25,36 @@ inline bool even(int n) {
 // For each couple i,j the sum of elements between i and j can be found by:
 // \sum_{n=i}^j bits[i] = s[j]-s[i]
 //
-// For evey pair i and j, compute the sum and check whether it's even.
-// If so, the ncount it.
+// For a given j:
+// - if s[j] is even, s[j]-s[i] is only going to be even is s[i] is also even
+// - if s[j] is odd, s[j]-s[i] is only going to be even if s[i] is also odd
+//
+// Define e[j] as the number of even sums below j
+//
+// For a given j:
+// - if s[j] is even, count e[j] even sums.
+// - if s[j] is odd, count j-e[j] even sums.
 int solve(std::vector<int> bits) {
   int n = bits.size();
 
   std::vector<int> s(n+1);
   s[0] = bits[0];
-  for (int i = 1; i <= n; ++i) {
-    s[i] = s[i-1] + bits[i-1];
+  for (int i = 0; i < n; ++i) {
+    s[i+1] = s[i] + bits[i];
+  }
+
+  std::vector<int> e(n+1);
+  e[0] = 0;
+  for (int i = 0; i < n; ++i) {
+    e[i+1] = e[i] + even(s[i]);
   }
 
   int even_count = 0;
-  for (int i = 0; i <= n; ++i) {
-    for (int j = i+1; j <= n; ++j) {
-      if (even(s[j] - s[i])) { ++even_count; }
+  for (int j = 0; j <= n; ++j) {
+    if (even(s[j])) {
+      even_count += e[j];
+    } else {
+      even_count += j - e[j];
     }
   }
 
