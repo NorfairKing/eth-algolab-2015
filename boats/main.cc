267b90b2cdec7af56ba857d9cd7cc0ab1a44ce82
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-25 10:41:38 +0200

    a better solution to the boats problem

--- boats/main.cc
+++ boats/main.cc
@@ -8,13 +8,8 @@ struct wizard {
   int ringpos;
 };
 
-static int freept;
-
 bool operator < (const wizard& w1, const wizard& w2) {
-  // The furthest to the left that the end will be.
-  return std::max(freept + w1.boatlen, w1.ringpos) < std::max(freept + w2.boatlen, w2.ringpos);
-  //return (w1.ringpos - w1.boatlen) < (w2.ringpos - w2.boatlen);
-  //return w1.ringpos < w2.ringpos;
+  return w1.ringpos < w2.ringpos;
 }
 std::ostream& operator << (std::ostream &out, wizard& w) {
   out << "Boat length: " << w.boatlen << "  Ring pos: " << w.ringpos << std::endl;
@@ -22,19 +17,26 @@ std::ostream& operator << (std::ostream &out, wizard& w) {
 }
 
 int solve(std::vector<wizard>& ws) {
-
   int counter = 0;
-  freept = -INT_MIN;
+  int right = INT_MIN;
+  int second_right = INT_MIN;
 
+  std::sort(ws.begin(), ws.end());
   for (auto it = ws.begin(); it < ws.end(); ++it) {
-    std::sort(it, ws.end());
-    
-    int leftend = std::max(freept, it->ringpos - it-> boatlen);
-    int rightend = leftend + it->boatlen;
-    if (rightend > it->ringpos + it->boatlen) { continue; }
-
-    ++counter;
-    freept = rightend;
+    if (it->ringpos >= right) {
+      // We can just place the boat
+      second_right = right;
+      right = std::max(it->ringpos, right + it->boatlen);
+      ++counter;
+    } else {
+      int first_free = std::max(it->ringpos, second_right + it->boatlen);
+      if (first_free <= right) {
+        // Replace the last boat with this one.
+        right = first_free;
+      } else {
+        // Skip this boat
+      }
+    }
   }
   return counter;
 }
