d8d589e3752967a2aac2fcaed77dc8feb3bb39f1
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-10-08 10:28:24 +0200

    wrote up on almost antenna

--- almost-antenna/main.cc
+++ almost-antenna/main.cc
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <limits>
+
 #include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
 #include <CGAL/Min_circle_2.h>
 #include <CGAL/Min_circle_2_traits_2.h>
-#include <boost/optional.hpp>
+
+using namespace std;
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
 typedef CGAL::Min_circle_2_traits_2<K> Traits;
@@ -11,56 +14,50 @@ typedef CGAL::Min_circle_2<Traits> Min_circle;
 typedef K::Point_2 P;
 
 double ceil_to_double(const K::FT& x) {
-  double a = std::ceil(CGAL::to_double(x));
+  double a = ceil(CGAL::to_double(x));
   while (a < x) a += 1;
   while (a - 1 >= x) a -= 1;
   return a;
 }
 
-double solve(std::vector<P> homes) {
+double solve(vector<P> homes) {
   boost::optional<K::FT> smallest;
+  Min_circle mc(homes.begin(), homes.end(), true); // Randomize input.
 
-  int n = homes.size();
-  for (int i = 0; i < n; ++i) {
-    // swap i with last = n-1
-    P tmp = homes[i];
-    homes[i] = homes[n - 1];
-    homes[n - 1] = tmp;
+  K::FT min_dist = numeric_limits<double>::max();
 
-    // Get minimum circle for all except the ith one
-    Min_circle mc(homes.begin(), homes.end() - 1, true); // Randomize input.
-    if (smallest) {
-      if (mc.circle().squared_radius() < smallest.get()) {
-          smallest = mc.circle().squared_radius();
-      }
-    } else {
-      smallest = mc.circle().squared_radius();
+  for (auto it = mc.support_points_begin(); it < mc.support_points_end(); ++it) {
+    vector<P> copy(homes);
+    for (auto i = copy.begin(); i < copy.end(); ++i) {
+      if (*i == *it) { copy.erase(i); }
+    }
+
+    Min_circle c(copy.begin(), copy.end(), true); // Randomize input.
+
+    if (c.circle().squared_radius() < min_dist) {
+      min_dist = c.circle().squared_radius();
     }
   }
 
-  if (smallest) {
-    return ceil_to_double(sqrt(smallest.get()));
-  } else {
-    return 0;
-  }
+  return ceil_to_double(sqrt(min_dist));
 }
 
 int main() {
-  std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+  cout << setiosflags(ios::fixed) << setprecision(0);
 
   while (true) {
     int n;
-    std::cin >> n;
+    cin >> n;
     if (n == 0) { break; }
 
-    std::vector<P> homes;
+    vector<P> homes;
     for (int i = 0; i < n; ++i) {
       long x, y;
-      std::cin >> x >> y;
+      cin >> x >> y;
       P h(x, y);
       homes.push_back(h);
     }
 
-    std::cout << solve(homes) << std::endl;
+    cout << solve(homes) << endl;
   }
 }
