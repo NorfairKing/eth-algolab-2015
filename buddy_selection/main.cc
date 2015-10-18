5934e95fc3d2aac500407d270b529a2460470b1d
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-10-18 15:18:11 +0200

    solved the buddy problem

--- buddy_selection/main.cc
+++ buddy_selection/main.cc
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <set>
+
+#include <boost/config.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/max_cardinality_matching.hpp>
 
 using namespace std;
+using namespace boost;
 
-void print_student(vector<int>& student) {
-  int n = student.size();
-  for (int i = 0; i < n; ++i) {
-    cout << student[i] << " ";
+typedef adjacency_list<vecS, vecS, undirectedS> Graph;
+typedef graph_traits<Graph>::edge_descriptor Edge;
+typedef graph_traits<Graph>::vertex_descriptor Vertex;
+
+bool solve(int nr_students, vector<set<int>> students, int nr_chars, int min_chars) {
+  Graph g(nr_students);
+  for (int i = 0; i < nr_students; ++i) {
+    for (int j = i + 1; j < nr_students; ++j) {
+      Edge edge; bool success;
+      vector<int> intersection(nr_chars);
+      auto it = set_intersection(students[i].begin(), students[i].end()
+                               , students[j].begin(), students[j].end()
+                               , intersection.begin());
+      int int_size = it - intersection.begin();
+      if (int_size <= min_chars) { continue; }
+      tie(edge, success) = add_edge(i, j, g);
+    }
   }
-  cout << endl;
-}
 
-bool solve(vector<vector<int>> students, int total_chars, int min_chars) {
-  return true;
+  vector<Vertex> mate(nr_students);
+  checked_edmonds_maximum_cardinality_matching(g, &mate[0]);
+  
+  int size = matching_size(g, &mate[0]);
+  return size != (nr_students / 2);
 }
 
 int main() {
@@ -23,30 +43,24 @@ int main() {
     int n, c, f;
     cin >> n >> c >> f;
 
-    vector<vector<int>> students(n);
+    vector<set<int>> students(n);
     int wordcount = 0; // Zero indexed words
     map<string, int> strmap;
     for (int i = 0; i < n; ++i) {
-      vector<int> student(c);
+      set<int> student;
       for (int j = 0; j < c; ++j) {
         string s;
         cin >> s;
-        //cout << s << endl;
 
         if (strmap.find(s) == strmap.end()) { // Doesn't contain s yet
           strmap.insert(make_pair(s, wordcount));
           ++wordcount;
-          //cout << s << " didn't exist yet." << " adding as " << wordcount << endl;
         }
-        student[j] = strmap[s];
+        student.insert(strmap[s]);
       }
-      // print_student(student);
       students[i] = student;
     }
-    // cout << wordcount << endl;
-    bool result = solve(students, wordcount, f);
-
-    if (result) {
+    if (solve(n, students, c, f)) {
       cout << "optimal" << endl;
     } else {
       cout << "not optimal" << endl;
