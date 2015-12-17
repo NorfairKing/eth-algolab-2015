2497efa05685aa26a16ce7ebd7dc4bbb820bd3eb
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2015-12-17 22:33:39 +0100

    better solution to hiking maps

--- src/hiking-maps/main.cc
+++ src/hiking-maps/main.cc
@@ -43,92 +43,93 @@ void normalise(triangle& t) {
   if (!left_turn(t.s1.p1, t.s1.p2, t.s2.p1)) { swap(t.s1); }
   if (!left_turn(t.s2.p1, t.s2.p2, t.s3.p1)) { swap(t.s2); }
   if (!left_turn(t.s3.p1, t.s3.p2, t.s1.p1)) { swap(t.s3); }
-  assert(normal(t));
+  // assert(normal(t));
 }
 
 bool within(P& p, triangle& t) {
-  assert(normal(t));
+  // assert(normal(t));
   bool lt1 = !right_turn(t.s1.p1, t.s1.p2, p);
   bool lt2 = !right_turn(t.s2.p1, t.s2.p2, p);
   bool lt3 = !right_turn(t.s3.p1, t.s3.p2, p);
-  // cout << t.s1.p1 << ", " << t.s1.p2 << ", " << p << endl;
-  // cout << t.s2.p1 << ", " << t.s2.p2 << ", " << p << endl;
-  // cout << t.s3.p1 << ", " << t.s3.p2 << ", " << p << endl;
-  // cout << lt1 << " " << lt2 << " " << lt3 << endl;
   return lt1 && lt2 && lt3;
 }
 
 bool contained_in(segment& s, triangle& t) {
-  assert(normal(t));
+  // assert(normal(t));
   bool result = within(s.p1, t) & within(s.p2, t);
-  // cout << result << endl;
   return result;
 }
 
 
 int solve(int m, int n, vector<segment>& legs, vector<triangle>& triangles) {
   for (triangle& t: triangles) { normalise(t); }
-  for (triangle& t: triangles) { assert(normal(t)); }
+  // for (triangle& t: triangles) { assert(normal(t)); }
 
   int segs = m - 1;
+
+  vector<vector<int>> covered(n); // Key: triangle, Value: covered legs
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < segs; ++j) {
+      if (contained_in(legs[j], triangles[i])) {
+        covered[i].push_back(j);
+      }
+    }
+  }
+
   int min_dist = INT_MAX;
+
+  vector<int> covers(segs, 0); // covers[i]: number of traingles that cover segment i
+  int nr_covered = 0;
 
   int i = -1;
   int j = 0;
 
   bool state = true; // True is looking right, false is taking left.
-  vector<deque<int>> covering(segs); // triangles covering that segment
   while (true) {
     if (j > i) { state = true; }
     if (j >= n) { break; }
+
     if (state) {
-      //cout << "state=true" << endl;
+      // Go right, add a new triangle to the current set.
       if (i >= n - 1) {
         state = false;
         continue;
       }
       ++i;
-      for (int k = 0; k < segs; ++k) {
-        if (contained_in(legs[k], triangles[i])) {
-          covering[k].push_back(i);
+
+      // The new triangle added is triangle i.
+      int nr_covers = covered[i].size();
+      for (int k = 0; k < nr_covers; ++k) {
+        int leg = covered[i][k];
+
+        // If the number of covers was zero, it's going to be one so one more leg is covered.
+        if (covers[leg] == 0) {
+          ++nr_covered;
         }
+
+        ++covers[leg];
       }
     } else {
-      for (int k = 0; k < segs; ++k) {
-        if (contained_in(legs[k], triangles[j])) {
-          assert(covering[k].front() == j);
-          covering[k].pop_front();
+      // The triangle removed is triangle j.
+      int nr_covers = covered[j].size();
+      for (int k = 0; k < nr_covers; ++k) {
+        int leg = covered[j][k];
+
+        --covers[leg];
+
+        // If the number of covers has become zero then one less leg is now covered.
+        if (covers[leg] == 0) {
+          --nr_covered;
         }
       }
       ++j;
     }
 
-    //cout << "i: " << i << "  j: " << j << endl;
-    bool all_covered = true;
-    for (int k = 0; k < segs; ++k) {
-      if (covering[k].empty()) { // not all covered
-        all_covered = false;
-        break;
-      }
-  
-      //cout << k << ": ";
-      //for (auto it = covering[k].begin(); it < covering[k].end(); ++it){
-      //  cout << *it << " ";
-      //}
-      //cout << endl;
-    }
-    //cout << endl;
-    //cout << "i: " << i << "  j: " << j << "  segs: " << segs << "  n: " << n << endl;
-    if (all_covered) {
-      //cout << "all covered" << endl;
-      // all covered
+    if (nr_covered == segs) { // All covered
       min_dist = std::min(min_dist, i - j + 1);
-      //cout << "setting min_dist to: " << min_dist << endl;
-      state = false; // maybe less is fine too?
-      //cout << endl;
+      state = false; // Maybe less is fine too?
     } else {
-      //cout << " not all covered " << endl;
-      state = true; // look for better situation.
+      state = true; // Look for better situation.
     }
   }
 
@@ -137,7 +138,7 @@ int solve(int m, int n, vector<segment>& legs, vector<triangle>& triangles) {
 
 
 int main() {
-  std::ios_base::sync_with_stdio(false);
+  ios_base::sync_with_stdio(false);
 
   int cases;
   cin >> cases;
@@ -150,22 +151,17 @@ int main() {
     for (int i = 0; i < m; ++i) {
       long x, y;
       cin >> x >> y;
-      //cout << x << ", " << y << endl;
       P pi(x, y);
       ps.push_back(pi);
     }
-    //cout << endl;
 
     vector<segment> ss;
     for (int i = 0; i < m - 1; ++i) {
       segment s;
       s.p1 = ps[i];
       s.p2 = ps[i + 1];
-      //cout << si << endl;
       ss.push_back(s);
     }
-    //cout << endl;
-
 
     vector<triangle> ts;
     for (int i = 0; i < n; ++i) {
@@ -173,7 +169,6 @@ int main() {
       for (int j = 0; j < 6; ++j) {
         long x, y;
         cin >> x >> y;
-        //cout << x << ", " << y << endl;
         ps[j] = P(x, y);
       }
       segment s1, s2, s3;
@@ -189,11 +184,7 @@ int main() {
       t.s2 = s2;
       t.s3 = s3;
       ts.push_back(t);
-
-      //cout << endl;
     }
-    //cout << endl;
-
     cout << solve(m, n, ss, ts) << endl;
   }
 }
