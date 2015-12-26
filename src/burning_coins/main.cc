a919df57292236c307a37d88fa9578f85a77fde3
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2015-12-26 15:10:38 +0100

    sped up burning coins

--- src/burning_coins/main.cc
+++ src/burning_coins/main.cc
@@ -11,31 +11,31 @@ bool even(int n) {
 
 // Even turns are mine.
 int at_least(int turn, vector<int>& values, int i, int j, vector<vector<optional<int>>>& state) {
-  if (state[i][j]) {
-    return state[i][j].get();
-  } else {
-    if (i > j) { return 0; }
-    bool e = even(turn);
-    if (i == j) {
-      if (e) {
-        return values[i];
+  if (! state[i][j]) {
+    int val;
+    if (i > j) { val = 0; }
+    else {
+      bool e = even(turn);
+      if (i == j) {
+        if (e) {
+          val = values[i];
+        } else {
+          val = 0;
+        }
       } else {
-        return 0;
+        int nt = turn + 1;
+        int a1 = at_least(nt, values, i + 1, j, state);
+        int a2 = at_least(nt, values, i, j - 1, state);
+        if (e) {
+          val = max(values[i] + a1, values[j] + a2);
+        } else {
+          val = min(a1, a2);
+        }
       }
     }
-
-    int nt = turn + 1;
-    int a1 = at_least(nt, values, i + 1, j, state);
-    int a2 = at_least(nt, values, i, j - 1, state);
-    int val;
-    if (e) {
-      val = max(values[i] + a1, values[j] + a2);
-    } else {
-      val = min(a1, a2);
-    }
     state[i][j] = val;
-    return val;
   }
+  return state[i][j].get();
 }
 
 int solve(int n, vector<int>& values) {
